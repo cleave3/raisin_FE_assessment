1b80e85458c65d721538f7a90dbd277645ec45a9
cleave owhiroro <owhiroroeghele@gmail.com>
2023-11-23 06:35:02 +0100

    add index.html to clear ReactDOM.creatRoot console warning

--- src/entry.tsx
+++ src/entry.tsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.body).render(<App />)
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(<App />)
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-    mode: "development",
-    entry: "./src/entry.tsx",
-    devtool: "inline-source-map",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        modules: ["node_modules"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-            },
-            {
-              test: /\.(jsx|tsx)?$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-              }
-            },
-        ],
-    },
-    plugins: [new HtmlWebpackPlugin()],
-};
+  mode: 'development',
+  entry: './src/entry.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: ['node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
+      {
+        test: /\.(jsx|tsx)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+}
